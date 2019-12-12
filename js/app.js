46d0a85362a5516ad600271d3b2ebd3bd83aea66
Ansh Gandhi <agandhi543@gmail.com>
2019-12-12 12:53:02 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// adding an item to a to-do list
+document.getElementById("addlist").onclick = addList;
+document.getElementById("removelist").onclick = removeList;
+document.getElementById("completelist").onclick = completeList;
+
+const addList = function() {
+  do {
+  let message = window.prompt("Add to List: ");
+  if (message) {
+  let list = [message];
+}
+  while (message != null)
+}
+do {
+
+}
