910f15d86dc67475f856f613edae53e6e6fddbda
Guest1 <my@email.comgit config --globak username my>
2019-12-21 17:54:59 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("addList").onclick = addList;
+    document.getElementById("completeList").onclick = markComplete;
+    document.getElementById("priorityList").onclick = itemPriority;
+    document.getElementById("deleteList").onclick = deleteItem;
+}
+
+const addList = function() {
+const item = [];
+let canvas = document.getElementById("student-canvas-1");
+let ctx = canvas.getContext("2d");
+
+};
