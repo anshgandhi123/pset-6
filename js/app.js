cd7e16ca39ddd70a930b179661a00e692542d80c
Guest1 <my@email.comgit config --globak username my>
2019-12-25 18:48:29 -0500

    it won't work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-let button = window.getelementById("button");
+let button = document.getElementById("button");
+let searchbar = document.getelementById("searchbar");
+function searchBar() {
+  let message = window.onclick(message);
+}
+function addList() {
+  let list = new Array(message)
+}
+let message = console.log(list);
