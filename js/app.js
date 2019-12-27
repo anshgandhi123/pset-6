9b89ad4bd858c6b9c2496ea7574aa383f9ee97ac
Guest1 <my@email.comgit config --globak username my>
2019-12-27 11:22:21 -0500

    commit number n

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-let button = document.getElementById("button");
-let searchbar = document.getelementById("searchbar");
+window.onload = function() {
+
+document.getElementById("button").onclick = button
+}
+
 function searchBar() {
+  let searchbar = document.getElementById("searchbar");
   let message = window.onclick(message);
 }
+
 function addList() {
   let list = new Array(message)
   let message = console.log(list);
 }
+
+function removeList() {
+  removeChild[]
+}
+
+function
