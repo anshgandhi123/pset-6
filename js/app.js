d1f907cb8dbb010a4c8d837d34a7a5057a9c8e45
Guest1 <my@email.comgit config --globak username my>
2019-12-31 18:14:50 -0500

    i seriously give up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 
+
+var firstLI = document.querySelector("li");
+firstLI.addEventListener("onclick", function() {
+    var item = document.getElementById("searchbar").value
+    textNode = document.createTextNode(input.value),
+    liNode = document.createElement("li")
+    newItem.append(text)
+    document.getElementById("addElement").append(newItem)
+});
+
+
+/*
+function addElement() {
+
+  var text = document.createTextNode(item)
+  var newItem = document.createElement("li")
+
+}
+*/
+/*
+  var input = document.getElementById("searchbar").value;
+  function addElement() {
+  var listNode = document.getElementById("unorderedList"),
+      textNode = document.createTextNode(input.value),
+      liNode = document.createElement("li")
+      liNode.append(textNode);
+      listNode.append(liNode);
+}
+*/
