0afe0057e513808474f6722155e5e5c6c436aebd
Guest1 <my@email.comgit config --globak username my>
2019-12-31 17:06:40 -0500

    restart time number idk

--- js/app.js
+++ js/app.js
@@ -1,11 +1,1 @@
-window.onload = function () {
-  const button = document.getElementById("button");
-  const searchbar = document.getElementById("searchbar").value
-}
 
-let toDoList = [];
-let i = 0
-
-const addElement = function() {
-  
-}
