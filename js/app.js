4153740a06f2618cc573c7fcc15bbc82415f7f1d
Guest1 <my@email.comgit config --globak username my>
2019-12-31 15:43:12 -0500

    starting from scratch

--- js/app.js
+++ js/app.js
@@ -1,26 +1,11 @@
 window.onload = function () {
   const button = document.getElementById("button");
-  const list = document.getElementById("ul");
+  const searchbar = document.getElementById("searchbar").value
 }
 
-function addElement() {
-  var li = document.createElement("li");
-  var searchbar = document.getElementById("searchbar").value
-  var text = document.createTextNode(searchbar);
-  li.appendChild(text)
-  document.getElementById("unorderedList").appendChild(li);
-  document.getElementById("searchbar").value = "";
+let toDoList = [];
+let i = 0
 
-  var span = document.createElement("SPAN");
-  var secondText = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(secondText);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-}
-  }
+const addElement = function() {
+  
 }
