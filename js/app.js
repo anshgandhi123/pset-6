0d2e5612c9dea02bffae531ecab3c7bc7e2e6fbf
Ansh Gandhi <55456749+anshgandhi123@users.noreply.github.com>
2020-01-03 11:51:35 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,38 @@
 window.onload = function() {
 document.getElementById("button").onclick = addElement;
 //  document.getElementById("fa fa close").onclick = deleteElement;
+document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+      addElement()
+    }
+  };
 }
 let toDo = [];
 
 const addElement = function() {
-  var input = document.getElementById("searchbar").value
+  var input = document.getElementById("searchbar").value;
   var liNode = document.createElement("li");
   var listNode = document.getElementById("unorderedList");
   liNode.append(input);
   listNode.append(liNode);
   toDo.push(input);
+  
+  var exclamation = document.createElement("button")
+  exclamation.id = exclamation
+  exclamation.value = "Priority"
+  exclamation.setAttribute("class", "priority");
+  var done = document.createElement("button")
+  done.id = done
+  done.value = "Complete"
+  done.setAttribute("class", "done");
+  var close = document.createElement("button")
+  close.id = close
+  close.value = "Delete"
+  close.setAttribute("class", "close");
+  liNode.append(exclamation)
+  liNode.append(done);
+  liNode.append(close);
+  
   if (input === "") {
     // on purpose
   }
@@ -22,16 +44,10 @@ const addElement = function() {
       remove: null
     }
     }
+    document.getElementById("searchbar").value = "";
   };
 
-// fix enter key properly it still doesn't work idk why... :(
-  document.body.onkeyup = function(e) {
-    if (e.keyCode == 13) {
-      addElement();
-    }
-  };
 
-/* deleteItem = (delete) => {
-
+deleteItem = (delete) => {
+  
 }
-*/
