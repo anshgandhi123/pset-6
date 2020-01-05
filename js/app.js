2d15b492e0eca2d5239b2bb3f205d078207b7e3a
Guest1 <my@email.comgit config --globak username my>
2020-01-05 12:29:05 -0500

    it finally worksgit status!

--- js/app.js
+++ js/app.js
@@ -1,231 +1,151 @@
-// NEED TO FIX EMPTY ENTER KEY THING AS IT STILL GOES THROUGH
-// NEED TO FIX DELETE ITEM THING TOO
+let toDo = []
+var addButton = document.getElementById("addButton");
+var row = document.getElementsByClassName("row");
+let priorityButton = document.getElementsByClassName("priorityButton");
+let completeButton = document.getElementsByClassName("completeButton");
+let removeButton = document.getElementsByClassName("removeButton");
+let items = document.getElementsByClassName("toDoList")
+
+let elementPriorities;
 
 window.onload = function() {
-document.getElementById("button").onclick = addElement;
-document.getElementsByClassName("X").onclick = deleteElement;
-
-document.body.onkeyup = function(e) {
-    if (e.keyCode == 13) {
-      addElement();
-    }
-  };
-};
-let toDo = [];
-let removeButton = document.getElementsByClassName("X").onclick
+  document.getElementById("table").onmouseover = start;
+}
 
 const addElement = function() {
-  var input = document.getElementById("searchbar").value;
-  var liNode = document.createElement("li");
-  var listNode = document.getElementById("unorderedList");
+  let input = document.getElementById("searchbar").value;
+  if (input === "") {
 
-  if (input === '') {
-// on purpose
   }
   else {
-    let toDoList = {
-      task: input,
-      complete: false,
-      priority: false,
-      remove: null
-    }
-  };
-
-  liNode.append(input);
-  listNode.append(liNode);
-  toDo.push(input);
-
-
-  var exclamation = document.createElement("button");
-  exclamation.id = exclamation;
-  exclamation.value = "Priority";
-  exclamation.setAttribute("class", "priority");
-  exclamation.innerHTML = "!";
-  var done = document.createElement("button");
-  done.id = done;
-  done.value = "Complete";
-  done.setAttribute("class", "fa fa-check");
-  var remove = document.createElement("button");
-  remove.id = remove;
-  remove.value = "Delete";
-  remove.setAttribute("class", "X");
-  remove.innerHTML = "X";
-  liNode.append(exclamation);
-  liNode.append(done);
-  liNode.append(remove);
-  document.getElementById("searchbar").value = "";
-
-  };
-
-  const deleteElement = function() {
-  var remove = false;
-  for (let i = 0; i < removeButton.length; i++) {
-    removeButton[i].onclick = function() {
-        remove = true;
-        let removeElement = listNode[i];
-        removeElement.remove();
-        toDo.splice(i, 1);
-    };
-    if (remove) {
-        break;
-    }
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-window.onload = function() {
-document.getElementById("button").onclick = addElement;
-document.body.onkeyup = function(e) {
-    if (e.keyCode == 13) {
-      addElement();
-    }
-  };
-};
-
-let toDo = [];
-let addButton = document.getElementById("button");
-let row = document.getElementsByClassName("row");
-let removeButton = document.getElementById("removeButton");
-let completeButton = document.getElementById("completeButton");
-let priorityButton = document.getElementById("priorityButton");
-let itemsInList = document.getElementsByClassName("toDoList");
-let a = 0;
-let i = 0;
-
-
-const addElement = function() {
-  var input = document.getElementById("searchbar").value;
-  if (input === '') {
-// on purpose
-  }
-  else {
-    var toDoList = {
-      task: input,
-      complete: false,
-      priority: false,
-      htmlRow: null,
-      text: null,
-      priorityButton: null,
-      removeButton: null,
-    };
-  toDo.push(input);
-
-  var value = toDo.indexOf(toDoList);
-
-  toDo[value].htmlRow = document.createElement("tr");
-  toDo[value].htmlRow.setAttribute("class", "row");
-  document.getElementById("table").append(toDo[value].htmlRow);
-
-  toDo[value].htmlPriorityButton = document.createElement("td");
-  toDo[value].htmlPriorityButton.setAttribute("class", "priorityButton");
-  toDo[value].htmlPriorityButton.innerHTML = "!";
-
-  row[value].append(toDo[value].htmlPriorityButton);
-
-  toDo[value].htmlText = document.createElement("td");
-  toDo[value].htmlText.innerHTML = toDo[value].task;
-  toDo[value].htmlText.setAttribute("class", "toDoList");
-
-  row[value].append(toDo[value].htmlText);
-
-  toDo[value].htmlCompleteButton = document.createElement("td");
-  toDo[value].htmlCompleteButton.innerHTML = "&#x2713;";
-  toDo[value].htmlCompleteButton.setAttribute("class", "complete_button");
-
-  row[value].append(toDo[value].htmlCompleteButton);
-
-  toDo[value].htmlRemoveButton = document.createElement("td");
-  toDo[value].htmlRemoveButton.setAttribute("class", "remove_button");
-  toDo[value].htmlRemoveButton.innerHTML = "X";
-
-  row[value].append(toDo[value].htmlRemoveButton);
+      let toDoList = {
+          task: input,
+          priority: false,
+          complete: false,
+          html_row: null,
+          html_priorityButton: null,
+          html_text: null,
+          html_removeButton: null
       }
-  document.getElementById("searchbar").value = "";
-  };
-*/
-/*
-  const deleteElement = function() {
+
+      toDo.push(toDoList);
+      let value = toDo.indexOf(toDoList);
+
+      toDo[value].htmlRow = document.createElement("tr");
+      toDo[value].htmlRow.setAttribute("class", "row");
+      document.getElementById("table").append(toDo[value].htmlRow);
+
+      toDo[value].htmlPriorityButton = document.createElement("td");
+      toDo[value].htmlPriorityButton.setAttribute("class", "priorityButton");
+      toDo[value].htmlPriorityButton.innerHTML = "!";
+
+      row[value].append(toDo[value].htmlPriorityButton);
+
+      toDo[value].htmlText = document.createElement("td");
+      toDo[value].htmlText.innerHTML = toDo[value].task;
+      toDo[value].htmlText.setAttribute("class", "toDoList");
+
+      row[value].append(toDo[value].htmlText);
+
+      toDo[value].htmlCompleteButton = document.createElement("td");
+      toDo[value].htmlCompleteButton.innerHTML = "&#x2713;";
+      toDo[value].htmlCompleteButton.setAttribute("class", "completeButton");
+
+      row[value].append(toDo[value].htmlCompleteButton);
+
+      toDo[value].htmlRemoveButton = document.createElement("td");
+      toDo[value].htmlRemoveButton.setAttribute("class", "removeButton");
+      toDo[value].htmlRemoveButton.innerHTML = "X";
+
+      row[value].append(toDo[value].htmlRemoveButton);
+    }
+    document.getElementById("searchbar").value = "";
+};
+
+const deleteElement = function() {
   var remove = false;
   for (let i = 0; i < removeButton.length; i++) {
     removeButton[i].onclick = function() {
         remove = true;
-        let removeElement = listNode[i];
-        toDo.splice(i, 1);
+        let removeElement = row[i];
         removeElement.remove();
+        toDo.splice(i, 1);
     };
     if (remove) {
         break;
     }
   }
-};
+}
 
 const completeElement = function() {
-var finish = false;
-for (let x = 0; x < completeButton.length; x++) {
-  complete[x].onclick = function() {
-     if (toDo[x].complete == false) {
-       finish = true;
-       completeItem[x].style.setProperty("text-decoration", "line-through");
-       completeItem[x].style.backgroundColor = "#baff66";
-       completeButton[x].style.backgroundColor = "#baff66";
-       toDo[x].complete = true;
+  var completed = false;
+  for (let x = 0; x < completeButton.length; x++) {
+    completeButton[x].onclick = function() {
+       if (toDo[x].complete == false) {
+         completed = true;
+         items[x].style.setProperty("text-decoration", "line-through");
+         items[x].style.backgroundColor = "lightgreen";
+         completeButton[x].style.backgroundColor = "lightgreen";
+         toDo[x].complete = true;
+       }
+       else if (toDo[x].complete == true) {
+         completeButton[x].style.backgroundColor = "white";
+         items[x].style.setProperty("text-decoration", "none");
+         items[x].style.backgroundColor = "white";
+         toDo[x].complete = false;
+       }
+     };
+     if (completed) {
+       break;
      }
-     else if (to[x].complete == true) {
-       completeButton[x].style.backgroundColor = "white";
-       completeItem[x].style.setProperty("text-decoration", "none");
-       completeItem[x].style.backgroundColor = "white";
-       toDo[x].complete = false;
-     }
-   };
-   if (finish) {
-     break;
-   }
+  }
 }
-}
-*/
-/*
-addButton.onclick = addElement;
 
-document.getElementById("searchbar").addEventListener("keyup", function(e) {
-  if (e.keyCode === 13) {
-    document.getElementById("button").click();
+const prioritizeElement = function() {
+  var prioritized = false;
+  for (let z = 0; z < priorityButton.length; z++) {
+    priorityButton[z].onclick = function () {
+      if (toDo[z].priority == false) {
+        elementPriorities = row[z]
+        prioritized = true;
+        priorityButton[z].style.backgroundColor = "yellow";
+        row[0].before(elementPriorities);
+        toDo[z].priority = true;
+
+        const move = toDo[z];
+
+        toDo.splice(z, 1);
+        toDo.unshift(move);
+        prioritized = true;
+      }
+      else if (toDo[z].priority) {
+        elementPriorities = row[z]
+        priorityButton[z].style.backgroundColor = "white";
+        row[toDo.length - 1].after(elementPriorities);
+        toDo[z].priority = false;
+
+        let element_move = toDo[z];
+        toDo.splice(z, 1);
+        toDo.push(element_move);
+        prioritized = true;
+      }
+    };
+    if (prioritized) {
+      break;
+    }
+  }
+}
+
+const start = function() {
+  deleteElement();
+  completeElement();
+  prioritizeElement();
+}
+
+addButton.onclick = addElement
+
+document.getElementById("searchbar").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    document.getElementById("addButton").click();
   }
 });
-*/
